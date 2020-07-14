d4c20f52148226d549df2539923511211ec6ffe2
Luka Buzaladze <buzaladzeluka@gmail.com>
2020-07-13 22:23:47 -0400

    finished lab

---
+++ contracts/Storage.sol
@@ -0,0 +1,21 @@
+pragma solidity >0.5.0;
+
+contract Storage {
+    bytes4 bytes4data = 0xaabbccdd;
+    uint72 uintdata = 0x123456;
+    bool booldata = true;
+    address addrdata = 0xdC962cEAb6C926E3a9B133c46c7258c0E371b82b;
+
+    function getData()
+        public
+        view
+        returns (
+            bytes4,
+            uint72,
+            bool,
+            address
+        )
+    {
+        return (bytes4data, uintdata, booldata, addrdata);
+    }
+}
